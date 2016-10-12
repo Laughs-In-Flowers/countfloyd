d513c42ee323304be4f29fa79ed08dbaffad56f6
blueblank <blueblank@gmail.com>
2016-10-12 17:02:42 -0400

    update example

--- example/main.go
+++ example/main.go
@@ -11,10 +11,10 @@ var start, populate, stop *exec.Cmd
 
 func init() {
 	cd, _ := os.Getwd()
-	start = exec.Command("cfc", "start")
-	populate = exec.Command("cfc", "-logFormatter", "stdout", "populate", "-dir", cd)
+	start = exec.Command("countfloyd", "start")
+	populate = exec.Command("countfloyd", "-logFormatter", "stdout", "populate", "-featuresDir", cd)
 	populate.Stdout = os.Stdout
-	stop = exec.Command("cfc", "stop")
+	stop = exec.Command("countfloyd", "stop")
 }
 
 func main() {
@@ -24,7 +24,7 @@ func main() {
 	populate.Start()
 	populate.Wait()
 	for i := 0; i <= 100; i += 2 {
-		q := exec.Command("cfc", "-logFormatter", "stdout", "apply", "-number", fmt.Sprintf("%d", i), "-features", "motivation")
+		q := exec.Command("countfloyd", "-logFormatter", "stdout", "apply", "-number", fmt.Sprintf("%d", i), "-features", "motivation")
 		q.Stdout = os.Stdout
 		q.Start()
 		q.Wait()
